a1f40a89772c62e8cdcaac2b02a1acaa3ca5f05b
Aleksandr Vladimirskiy <aleksandr@butchershopcreative.com>
2012-05-04 16:47:58 -0700

    complete layout

--- soundcloud/example/application.js
+++ soundcloud/example/application.js
@@ -6,53 +6,57 @@ $(function() {
       redirect_uri: "http://localhost:8000/soundcloud/example/"
     });
         
-    SC.get("/tracks?genres=dubstep&order=hotness", {limit: 5}, function(tracks){
-       console.log(tracks);
+    SC.get("/tracks?genres=dubstep&order=hotness", {limit: 10}, function(tracks){
+
       var x = 75;
-      var y = 0;
+      var y = 240;
+      var width = 640;
+      var height = 640;
+
       $(tracks).each(function(n, track) {
         $(track).attr('x', x);
         x = x + 128;
-        // "y" is being manipulated by comment count
         $(track).attr('y', y);
-        y = track.comment_count / 3;
-        console.log(y);
+        $(track).attr('radius', track.favoritings_count / 75);
+        $(track).attr('value', track.favoritings_count);
       });
 
       var svg = d3.select("#chart").append("svg")
         .attr("width", "100%")
-        .attr("height", "100%");
+        .attr("height", "100%")
+        .attr("class", "bubble");
 
-      var r = 50;
-      // The following produces different radii based off of how many Favoritings a track has
-      // The more favorites, the bigger the radii
-      $(tracks).each(function(n, track) {
-        r = track.favoritings_count / 75;
-        $(track).attr('r', r);
-        });
-        
+      var bubble = d3.layout.pack()
+        .sort(null)
+        .size([width, height]);
+
       var node = svg.selectAll("g.node")
-        .data(tracks);
+        .data(bubble.nodes({children: tracks}));
 
       node.enter().append('g')
         .attr('class', 'node')
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
       node.append("circle")
-        .style("fill", function(d) { return "#" + d.duration })
+        .style("fill", function(d) { return "#fff"; })
         .style("stroke","#000")
         .style("stroke-width", "3px")
         .attr("class", "track")
-        .attr("r", function(d) { return d.r })
+        .attr("r", function(d) { return d.radius; });
 
+      // var children = node.selectAll('')
       node.append("text")
         .attr("text-anchor", "middle")
         .attr("dy", ".2em")
         .attr("color", "255,255,240") //font color wtf?
         // Returning the track title - shortened using substring
         .text(function(track) {
-          return track.title.substring(0, track.r / 5) + "...";
-      });
+          if (track.title) {
+            return track.title.substring(0, 12);
+          } else {
+            return null;
+          }
+        });
 
       node.exit().remove();
     });
