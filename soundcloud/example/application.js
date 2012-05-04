81c8baae404a06bf87bd9dacebb969213991ca59
Aleksandr Vladimirskiy <aleksandr@butchershopcreative.com>
2012-05-04 10:09:40 -0700

    re-factor to draw text and circles

---
+++ soundcloud/example/application.js
@@ -0,0 +1,38 @@
+$(function() {
+    // Check out http://mbostock.github.com/d3/tutorial/circle.html
+    SC.initialize({
+      client_id: "7kIeyF5f2ETFo1fEWKwNQ",
+      redirect_uri: "http://localhost:8000/soundcloud/example/",
+    });
+        
+    SC.get("/tracks?genres=dubstep&order=hotness", {limit: 5}, function(tracks){
+      var svg = d3.select("#chart").append("svg")
+            .attr("width", 640)
+            .attr("height", 480);
+          
+      var y = 500;
+      var x = 1000;
+      
+      var node = svg.selectAll("g.node")
+            .data(tracks);
+    node.enter().append('g')
+      .attr('class', 'node');
+    node.append("title").text('something');
+      node.append("circle")
+            .style("fill", "steelblue")
+            .style("stroke","#000")
+            .style("stroke-width", "3px")
+            .attr("class", "track")
+            .attr("cx", function() {
+                return Math.random() * x;}) // produces random x position
+            .attr("cy", function() {
+                return Math.random() * y;}) // produces random y position
+            .attr("r", 50);
+    node.append("text")
+      .attr("text-anchor", "middle")
+      .attr("dy", ".3em")
+      .text('Another');
+    node.exit().remove();
+    });
+});
+
