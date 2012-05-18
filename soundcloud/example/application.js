2dda19bf7ef089623a2fffe1fc51e39a6dd7360e
Aleksandr Vladimirskiy <aleksandr@butchershopcreative.com>
2012-05-17 22:03:24 -0700

    merge

--- soundcloud/example/application.js
+++ soundcloud/example/application.js
@@ -17,7 +17,7 @@ $(function() {
         $(track).attr('x', x);
         x = x + 128;
         $(track).attr('y', y);
-        $(track).attr('radius', track.favoritings_count / 75);
+        $(track).attr('radius', track.favoritings_count / 60);
         $(track).attr('value', track.favoritings_count);
       });
 
@@ -37,22 +37,31 @@ $(function() {
         .attr('class', 'node')
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-      node.append("circle")
-        .style("fill", function(d) { return "#fff"; })
+      node.append("a")
+        .attr('xlink:href', function(track) { return track.permalink_url; });
+
+      node.selectAll('a')
+        .append("image")
+        .attr('xlink:href', function(track) { return track.artwork_url; })
+        .attr('width', 100)
+        .attr('height', 100);
+        // .transform('translate', function(d) { return "translate(" + d.x - 200 + "," + d.y - 200 + ")"; });
+
+      node.selectAll('a')
+        .append("circle")
+        .style("fill", "transparent")
         .style("stroke","#000")
         .style("stroke-width", "3px")
         .attr("class", "track")
         .attr("r", function(d) { return d.radius; });
 
-      // var children = node.selectAll('')
       node.append("text")
         .attr("text-anchor", "middle")
-        .attr("dy", ".2em")
-        .attr("color", "255,255,240") //font color wtf?
+        .attr("dy", ".5em")
         // Returning the track title - shortened using substring
         .text(function(track) {
           if (track.title) {
-            return track.title.substring(0, 12);
+            return track.title.substring(0, 24);
           } else {
             return null;
           }
